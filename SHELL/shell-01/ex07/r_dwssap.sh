4e982bf696b01cb8892c3043a3a441daa48816ff
Manuel Traballoni <70847897+perfection1stthe@users.noreply.github.com>
2020-11-25 22:35:58 +0100

    Add files via upload
    shell 00 and shell 01 upload

---
+++ SHELL/shell-01/ex07/r_dwssap.sh
@@ -0,0 +1,2 @@
+FT_QUIT=$(($FT_LINE2+1)
+cat /etc/passwd | grep -v '^#' | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 P;$FT_QUIT q" | sed 's/$/,/g' | tr '\n' ' ' | sed 's/, $/./' | tr -d '\n'
